d741abfd935ea183dc5b6989243464cc32ea87df
Marius Stoica <s128273@ap.be>
2022-11-09 18:58:00 +0100

    Added stop when hero attacks

--- Game development project/Classes/KeyboardReader.cs
+++ Game development project/Classes/KeyboardReader.cs
@@ -13,48 +13,57 @@ namespace Game_development_project.Classes
     {
         public static SpriteStates SpriteState { get; set; }
         public static Direction SpriteDirection { get; set; }
+        
 
         public Vector2 ReadInput()
         {
+            bool attacking = false;
             KeyboardState state = Keyboard.GetState();
             Vector2 direction = Vector2.Zero;
-            if (state.IsKeyDown(Keys.Left))
-            {
-                direction.X -= 1;
-                SpriteState = SpriteStates.Left;
-                SpriteDirection = Direction.Left; 
-                
-                
-            }
-            else if (state.IsKeyDown(Keys.Right))
-            {
-                direction.X += 1;
-                SpriteState = SpriteStates.Right;
-                SpriteDirection = Direction.Right;
 
-            }
-            else if (state.IsKeyDown(Keys.Up))
+            if (!attacking)
             {
-                direction.Y -= 1;
-                SpriteState = SpriteStates.Up;
+                if (state.IsKeyDown(Keys.Left))
+                {
+                    direction.X -= 1;
+
+                    SpriteState = SpriteStates.Left;
+                    SpriteDirection = Direction.Left;
 
 
-            }
-            else if (state.IsKeyDown(Keys.Down))
-            {
-                direction.Y += 1;
-                SpriteState = SpriteStates.Down;
+                }
+                else if (state.IsKeyDown(Keys.Right))
+                {
+                    direction.X += 1;
+                    SpriteState = SpriteStates.Right;
+                    SpriteDirection = Direction.Right;
 
-            }
-            else if (state.IsKeyDown(Keys.F))
-            {
-                SpriteState = SpriteStates.Attack;
-            }
-            else
-            {
-                SpriteState = SpriteStates.Idle;
-            }
+                }
+                else if (state.IsKeyDown(Keys.Up))
+                {
+                    direction.Y -= 1;
+                    SpriteState = SpriteStates.Up;
+                   
 
+                }
+                else if (state.IsKeyDown(Keys.Down))
+                {
+                    direction.Y += 1;
+                    SpriteState = SpriteStates.Down;
+
+                }
+
+               else if (state.IsKeyDown(Keys.F))
+                {
+                    SpriteState = SpriteStates.Attack;
+                    attacking = true;
+                }
+
+                else
+                {
+                    SpriteState = SpriteStates.Idle;
+                }
+            }
             
             return direction;
         }
