d15f93fe0ca95413043a442972d7e0817d99515e
MariusSpaceEngineer <marius.stoica@student.ap.be>
2022-12-09 13:44:49 +0100

    Added CastleGoundBlock class

---
+++ Game development project/Classes/Level-Design/TypeBlocks/CastleGroundBlock.cs
@@ -0,0 +1,21 @@
+﻿using Default_Block;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_development_project.Classes.Level_Design.TypeBlocks
+{
+    internal class CastleGroundBlock : Block
+    {
+        public CastleGroundBlock(Rectangle newrectangle) : base(newrectangle)
+        {
+            texture = Content.Load<Texture2D>("Castle_Ground");
+            this.Rectangle = newrectangle;
+
+        }
+    }
+}
