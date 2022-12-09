416a13d118145017f8456b6602381c9aca13a13e
MariusSpaceEngineer <marius.stoica@student.ap.be>
2022-12-09 13:44:01 +0100

    Created level 2

---
+++ Game development project/Classes/Level-Design/Level2/Level2_BlockFactory.cs
@@ -0,0 +1,31 @@
+﻿using Default_Block;
+using Game_development_project.Classes.Level_Design.Level;
+using Game_development_project.Classes.Level_Design.Level1;
+using Game_development_project.Classes.Level_Design.TypeBlocks;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_development_project.Classes.Level_Design.Level2
+{
+    internal class Level2_BlockFactory : BlockFactory
+    {
+        public Block CreateBlock(int number, Rectangle rectangle)
+        {
+            Block block = null;
+
+            if (number == 1)
+            {
+                block = new GrassBlock(rectangle);
+            }
+            else if (number == 2)
+            {
+                block = new DirtBlock(rectangle);
+            }
+            return block;
+        }
+    }
+}
