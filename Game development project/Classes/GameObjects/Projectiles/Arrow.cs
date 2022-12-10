365b2d337381a8f98ec2f7ca5b03f25e3091a584
MariusSpaceEngineer <marius.stoica@student.ap.be>
2022-12-10 15:12:14 +0100

    Added Projectile classes

---
+++ Game development project/Classes/GameObjects/Projectiles/Arrow.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_development_project.Classes.GameObjects.Projectiles
+{
+    internal class Arrow : Projectile
+    {
+        public Arrow(Texture2D texture) : base(texture)
+        {
+
+        }
+      
+    }
+}
---
+++ Game development project/Classes/GameObjects/Projectiles/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_development_project.Classes.GameObjects.Projectiles
+{
+    internal abstract class Projectile : GameObject
+    {
+        private float _timer;
+        public float LifeSpan = 0f;
+
+        public Projectile(Texture2D texture)
+          : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer >= LifeSpan)
+                IsRemoved = true;
+
+            Position += Direction * LinearVelocity;
+        }
+    }
+}
